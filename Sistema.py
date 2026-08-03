87c5c3ab5f56aaa4567012653fd537034b047bec
fabio-bovanini <122551038+fabio-bovanini@users.noreply.github.com>
2026-08-03 14:14:34 -0400

    creacion de main y sistema (supermercado en el ejercicio de las prepas de leo)

---
+++ Sistema.py
@@ -0,0 +1,10 @@
+import json
+
+class Sistema:
+    def __init__(self, ruta_json):
+        self.ruta_json = ruta_json
+
+        
+    
+
+        
---
+++ main.py
@@ -0,0 +1,10 @@
+from lectura_json import CargadorDatos
+from municipio import Municipio
+from localidad import Localidad
+from Sistema import Sistema
+
+def main(self):
+
+    sistema = Sistema("zonas_caracas.json")
+    
+
